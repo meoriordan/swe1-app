f5d6ab7220b991d4e6c047d06bcdb16fec3b136d
meoriordan <meoriordan@gmail.com>
2022-10-04 10:23:33 -0400

    change db to postgres

--- ebdjango/ebdjango/settings.py
+++ ebdjango/ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ebdjango-dev.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['ebdjango-dev.us-east-1.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
@@ -74,12 +74,30 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS+PORT']
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'pollls_app',
+            'USER': 'postgres',
+            'PASSWORD': config("DB_PASSWORD"),
+            'HOST': 'localhost',
+            'PORT': '5433'
+        }
+    }
 
 
 # Password validation
