491f37e6a32f51e6ea5acff82cd2c741352d5103
meoriordan <meoriordan@gmail.com>
2022-10-06 13:02:44 -0400

    fix admin login for eb

--- ebdjango/polls/management/commands/createsu.py
+++ ebdjango/polls/management/commands/createsu.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
+from decouple import config
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
@@ -11,6 +11,6 @@ class Command(BaseCommand):
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
                 username='admin',
-                password='complexpassword123'
+                password=config("SU_PASSWORD")
             )
         print('Superuser has been created.')
